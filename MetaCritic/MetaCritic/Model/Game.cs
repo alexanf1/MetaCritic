74a8a8f0e7a054e9e0b6fa8ad24be2e9b57dccf4
Alex Flores <alexflores951@gmail.com>
2021-08-20 12:40:11 -0700

    Added new exception 'MetaCriticInvalidGameException'
    - Added more commentary for Game Model

--- MetaCritic/MetaCritic/Model/Game.cs
+++ MetaCritic/MetaCritic/Model/Game.cs
@@ -1,28 +1,37 @@
 ﻿
+using MetaCritic.Exceptions;
+
 namespace MetaCritic.Model
 {
     using System;
 
+    /// <inheritdoc />
     public class Game : IGame
     {
         public Game(string name)
         {
             if (string.IsNullOrWhiteSpace(name))
             {
-                throw new ArgumentException("Name is required.", nameof(name));
+                throw new MetaCriticInvalidGameException($"{typeof(Game)} could not be created!",
+                    new ArgumentException("Name is required.", nameof(name)));
             }
 
             Name = name;
         }
 
+        /// <inheritdoc />
         public string Name { get; }
 
+        /// <inheritdoc />
         public string Platform { get; set; }
 
+        /// <inheritdoc />
         public DateTime? ReleaseDate { get; set; }
 
+        /// <inheritdoc />
         public int? Score { get; set; }
 
+        /// <inheritdoc />
         public string Description { get; set; }
     }
 }
